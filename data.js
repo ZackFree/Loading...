a9de1dabd31577a74ded98290ec70122f037dc30
Care Free <brainred6@gmail.com>
2021-04-12 17:42:06 +0530

    NEW WORLD ORDER

---
+++ data.js
@@ -0,0 +1,7 @@
+var header_data =
+{
+ Img:"assets/images/main.png",
+ Content:"...But living creatures are strange. They are made in such a way that they can actualize only what their minuscule minds wish for.",
+ Author:"--Aizen Sosuke"
+};
+
---
+++ index.js
@@ -0,0 +1,6 @@
+
+function headerData(){
+document.getElementById("header-img-content").src= header_data.Img;
+document.getElementById("header-text-content").innerHTML= header_data.Content;
+document.getElementById("header-quote-content").innerHTML= header_data.Author;
+}
